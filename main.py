7fef0fe98777c5d983d444b5b5f7b1bc2f7a30cd
Justin-Yuan <justin.zcyuan@gmail.com>
2017-01-21 21:08:03 -0500

    modified main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+""" 
+
+"""
+import 
+
+import encodings 
+
+# Step 1 - read in the source image and extra information 
+
+para = Encodings("example.jpg")
+
+
+# Step 2 - Generate music and store in .midi file 
+
+
+
